6d62839da408b14b171ad4cb79c1c6ddaa20a2e5
Will Fehrnstrom <wfehrnstrom@gmail.com>
2020-02-20 10:32:26 -0800

    Modify to match master files.

---
+++ src/JoinGame.svelte
@@ -0,0 +1,31 @@
+<script>
+    // Handler Funcs
+    export let createGame;
+    export let joinGame;
+    // Game ID value filled into form by user
+    export let game_id;
+</script>
+
+<div>
+    <div class='join-game'>
+        <form>
+            <label for="join-code"></label>
+            <input 
+                type="text" 
+                id="join-code" 
+                name="Join With Game Code" required 
+                bind:value={game_id}
+                minlength="8"
+                maxlength="8">
+        </form>
+        <button on:click={joinGame}>
+            Join Game With Code
+        </button>
+    </div>
+
+    <div class='create-game'>
+        <button on:click={createGame}>
+            Create a New Game
+        </button>
+    </div>
+</div>
---
+++ src/lobby.js
@@ -0,0 +1,7 @@
+import Lobby from './Lobby.svelte';
+
+const app = new Lobby({
+	target: document.body
+});
+
+export default app;
